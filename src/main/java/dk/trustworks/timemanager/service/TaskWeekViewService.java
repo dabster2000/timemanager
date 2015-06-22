a75936b948d187db4c82327b041e68d8489a5ab8
hans <hans.lassen@trustworks.dk>
2015-06-22 22:00:01 +0200

    Faster week loading with parallel processes

--- src/main/java/dk/trustworks/timemanager/service/TaskWeekViewService.java
+++ src/main/java/dk/trustworks/timemanager/service/TaskWeekViewService.java
@@ -17,6 +17,7 @@ import java.sql.SQLException;
 import java.time.DayOfWeek;
 import java.time.format.TextStyle;
 import java.util.*;
+import java.util.stream.StreamSupport;
 
 import static java.util.Calendar.*;
 
@@ -42,19 +43,23 @@ public class TaskWeekViewService extends DefaultLocalService {
         List<Map<String, Object>> taskWeekViews = new ArrayList<>();
         List<Map<String, Object>> weeks = weekRepository.findByWeekNumberAndYearAndUserUUIDOrderBySortingAsc(weekNumber, year, userUUID);
 
-        for (Map<String, Object> week : weeks) {
+        //for (Map<String, Object> week : weeks) {
+        StreamSupport.stream(weeks.spliterator(), true).map((week) -> {
             Object taskUUID = week.get("taskuuid");
             Map<String, Object> task = new TaskService().getOneEntity("tasks", taskUUID.toString());
+            /*
             boolean dublet = false;
             for (Map<String, Object> tempView : taskWeekViews) {
-                if(tempView.get("taskuuid").equals(taskUUID.toString())) dublet = true;
+                if (tempView.get("taskuuid").equals(taskUUID.toString())) dublet = true;
             }
-            if(dublet) continue;
+            //if(dublet) continue;
+            */
             Map<String, Object> taskWeekView = new HashMap<>();
             Map<String, Object> project = new ProjectService().getOneEntity("projects", task.get("projectuuid").toString());
             Map<String, Object> client = new ClientService().getOneEntity("clients", project.get("clientuuid").toString());
             taskWeekView.put("taskname", task.get("name") + " / " + project.get("name") + " / " + client.get("name"));
             taskWeekView.put("taskuuid", taskUUID.toString());
+            taskWeekView.put("sorting", week.get("sorting"));
 
             double budgetLeft = 0.0;
             try {
@@ -63,7 +68,7 @@ public class TaskWeekViewService extends DefaultLocalService {
                         .queryString("useruuid", userUUID)
                         .header("accept", "application/json")
                         .asJson();
-                budgetLeft = (double)jsonResponse.getBody().getObject().get("remaining");
+                budgetLeft = (double) jsonResponse.getBody().getObject().get("remaining");
             } catch (UnirestException e) {
                 log.error("LOG00780:", e);
             }
@@ -76,15 +81,22 @@ public class TaskWeekViewService extends DefaultLocalService {
             c.set(WEEK_OF_YEAR, weekNumber);
             c.set(YEAR, year);
 
-            for(int i = 0; i <7; i++) {
+            for (int i = 0; i < 7; i++) {
                 List<Map<String, Object>> works = workRepository.findByYearAndMonthAndDayAndTaskUUIDAndUserUUID(c.get(YEAR), c.get(MONTH), c.get(DAY_OF_MONTH), taskUUID.toString(), userUUID);
                 for (Map<String, Object> work : works) {
-                    taskWeekView.put(DayOfWeek.of(i+1).getDisplayName(TextStyle.FULL, Locale.ENGLISH).toLowerCase(), work.get("workduration"));
+                    taskWeekView.put(DayOfWeek.of(i + 1).getDisplayName(TextStyle.FULL, Locale.ENGLISH).toLowerCase(), work.get("workduration"));
                 }
                 c.add(DATE, 1);
             }
-            taskWeekViews.add(taskWeekView);
-        }
+            //taskWeekViews.add(taskWeekView);
+            return taskWeekView;
+        }).forEach(result -> taskWeekViews.add(result));
+            //return taskWeekView;
+        //System.out.println("taskWeekViews... = " + taskWeekViews.get(2));
+        taskWeekViews.sort((p1, p2) -> Integer.compare(Integer.parseInt(p1.get("sorting").toString()), Integer.parseInt(p2.get("sorting").toString())));
+        //}).forEach(result -> taskWeekViews.add(result));
+        //taskWeekViews.sort((e1, e2) -> Integer.compare((Integer) e1.get("sorting"),
+          //      (Integer) e2.get("sorting")));
 
         return taskWeekViews;
     }
