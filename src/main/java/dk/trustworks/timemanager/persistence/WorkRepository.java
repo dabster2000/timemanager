11510f9b4431d8c19edccda15488854225a2d746
hans <hans.lassen@trustworks.dk>
2015-06-22 11:22:02 +0200

    Changed database connection polling system to HikariCP

--- src/main/java/dk/trustworks/timemanager/persistence/WorkRepository.java
+++ src/main/java/dk/trustworks/timemanager/persistence/WorkRepository.java
@@ -40,64 +40,24 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00610:", e);
         }
         return new ArrayList<>();
-        /*
-        List<Map<String, Object>> result = new ArrayList<>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("select yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                    "from work yt inner join( " +
-                    "select uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                    "from work WHERE taskuuid LIKE ? " +
-                    "group by day, month, year " +
-                    ") ss on yt.month = ss.month and yt.year = ss.year and yt.day = ss.day and yt.created = ss.created and yt.taskuuid = ss.taskuuid and yt.useruuid = ss.useruuid;", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setString(1, taskuuid);
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     public List<Map<String, Object>> findByYear(String year) {
         log.debug("WorkRepository.findByYear");
         log.debug("year = [" + year + "]");
         try (org.sql2o.Connection con = database.open()) {
-            return getEntitiesFromMapSet(con.createQuery("SELECT yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                    "FROM work yt INNER JOIN( " +
-                    "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                    "FROM work WHERE year = :year " +
-                    "GROUP BY year) ss " +
-                    "ON yt.year = ss.year AND yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;")
+            return getEntitiesFromMapSet(con.createQuery("select yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
+                    "from work yt inner join ( " +
+                    "select uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
+                    "from work WHERE year LIKE :year " +
+                    "group by day, month, year, taskuuid, useruuid " +
+                    ") ss on yt.month = ss.month and yt.year = ss.year and yt.day = ss.day and yt.created = ss.created and yt.taskuuid = ss.taskuuid and yt.useruuid = ss.useruuid;")
                     .addParameter("year", year)
                     .executeAndFetchTable().asList());
         } catch (Exception e) {
             log.error("LOG00620:", e);
         }
         return new ArrayList<>();
-        /*
-        List<Map<String, Object>> result = new ArrayList<>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                    "FROM work yt INNER JOIN( " +
-                    "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                    "FROM work WHERE year = ? " +
-                    "GROUP BY year) ss " +
-                    "ON yt.year = ss.year AND yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setInt(1, Integer.parseInt(year));
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     public List<Map<String, Object>> findByYearAndUserUUID(String year, String userUUID) {
@@ -117,29 +77,6 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00630:", e);
         }
         return new ArrayList<>();
-        /*
-        List<Map<String, Object>> result = new ArrayList<>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                            "FROM work yt INNER JOIN( " +
-                            "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                            "FROM work WHERE useruuid LIKE ? AND year = ?" +
-                            "GROUP BY day, month, year) ss " +
-                            "ON yt.month = ss.month AND yt.year = ss.year AND yt.day = ss.day AND yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;",
-                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setString(1, userUUID);
-            stmt.setInt(2, Integer.parseInt(year));
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            resultSet.close();
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     public List<Map<String, Object>> findByYearAndMonth(String year, String month) {
@@ -159,30 +96,6 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00640:", e);
         }
         return new ArrayList<>();
-        /*
-        List<Map<String, Object>> result = new ArrayList<>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT yt.uuid, yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                            "FROM work yt INNER JOIN( " +
-                            "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                            "FROM work WHERE month = ? AND year = ? " +
-                            "GROUP BY day, month, year, taskuuid, useruuid) ss " +
-                            "ON yt.day = ss.day AND yt.month = ss.month AND yt.year = ss.year AND yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;",
-                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setInt(1, Integer.parseInt(month));
-            stmt.setInt(2, Integer.parseInt(year));
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            System.out.println("result = " + result);
-            resultSet.close();
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     public List<Map<String, Object>> findByYearAndMonthAndTaskUUIDAndUserUUID(String year, String month, String taskUUID, String userUUID) {
@@ -204,31 +117,6 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00650:", e);
         }
         return new ArrayList<>();
-        /*
-        List<Map<String, Object>> result = new ArrayList<>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                            "FROM work yt INNER JOIN( " +
-                            "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                            "FROM work WHERE taskuuid LIKE ? AND useruuid LIKE ? AND month = ? AND year = ? " +
-                            "GROUP BY day, month, year) ss " +
-                            "ON yt.month = ss.month AND yt.year = ss.year AND yt.day = ss.day AND yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;",
-                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setString(1, taskUUID);
-            stmt.setString(2, userUUID);
-            stmt.setInt(3, Integer.parseInt(month));
-            stmt.setInt(4, Integer.parseInt(year));
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            resultSet.close();
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     public List<Map<String, Object>> findByYearAndMonthAndDayAndTaskUUIDAndUserUUID(int year, int month, int day, String taskUUID, String userUUID) {
@@ -251,31 +139,6 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00660:", e);
         }
         return new ArrayList<>();
-        /*
-        List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                    "FROM work yt INNER JOIN( " +
-                    "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                    "FROM work WHERE taskuuid LIKE ? AND useruuid LIKE ? AND year = ? AND month = ? AND day = ? " +
-                    "GROUP BY day, month, year) ss " +
-                    "ON yt.month = ss.month AND yt.year = ss.year AND yt.day = ss.day AND yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;",
-                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setString(1, taskUUID);
-            stmt.setString(2, userUUID);
-            stmt.setInt(3, year);
-            stmt.setInt(4, month);
-            stmt.setInt(5, day);
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     public List<Map<String, Object>> findByTaskUUIDAndUserUUID(String taskUUID, String userUUID) {
@@ -295,28 +158,6 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00670:", e);
         }
         return new ArrayList<>();
-        /*
-        List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                            "FROM work yt INNER JOIN( " +
-                            "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                            "FROM work WHERE taskuuid LIKE ? AND useruuid LIKE ? " +
-                            "GROUP BY day, month, year) ss " +
-                            "ON yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;",
-                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setString(1, taskUUID);
-            stmt.setString(2, userUUID);
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     public List<Map<String, Object>> findByYearAndMonthAndTaskUUID(int year, int month, String taskUUID) {
@@ -337,29 +178,6 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00680:", e);
         }
         return new ArrayList<>();
-        /*
-        List<Map<String, Object>> result = new ArrayList<>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                    "FROM work yt INNER JOIN( " +
-                    "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                    "FROM work WHERE taskuuid LIKE ? AND month = ? AND year = ? " +
-                    "GROUP BY day, month, year) ss " +
-                    "ON yt.month = ss.month AND yt.year = ss.year AND yt.day = ss.day AND yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;",
-                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setString(1, taskUUID);
-            stmt.setInt(2, month);
-            stmt.setInt(3, year);
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     public List<Map<String, Object>> findByYearAndTaskUUIDAndUserUUID(int year, String taskUUID, String userUUID) {
@@ -380,42 +198,7 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00690:", e);
         }
         return new ArrayList<>();
-        /*
-        return new ArrayList<>();
-        List<Map<String, Object>> result = new ArrayList<>();
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT yt.month, yt.year, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                    "FROM work yt INNER JOIN( " +
-                    "SELECT uuid, month, year, day, workduration, taskuuid, useruuid, max(created) created " +
-                    "FROM work WHERE taskuuid LIKE ? AND useruuid LIKE ? AND year = ? " +
-                    "GROUP BY day, month, year) ss " +
-                    "ON yt.month = ss.month AND yt.year = ss.year AND yt.day = ss.day AND yt.created = ss.created AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid;",
-                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setString(1, taskUUID);
-            stmt.setString(2, userUUID);
-            stmt.setInt(3, year);
-            ResultSet resultSet = stmt.executeQuery();
-            result = getEntitiesFromResultSet(resultSet);
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
-
-    /*
-    select sum(workduration) sum from (
-    select yt.year, yt.month, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid
-    from work yt inner join(
-    select uuid, day, month, year, workduration, taskuuid, useruuid, max(created) created
-    from work WHERE taskuuid LIKE '217e5c68-eb72-40b6-a3d2-099c34f89405' and useruuid LIKE '7948c5e8-162c-4053-b905-0f59a21d7746'
-    group by month, year) ss
-    on yt.month = ss.month and yt.year = ss.year and yt.day = ss.day and yt.created = ss.created and yt.taskuuid = ss.taskuuid and yt.useruuid = ss.useruuid
-    ) we;
-     */
 
     public double calculateTaskUserTotalDuration(String taskUUID, String userUUID) {
         log.debug("WorkRepository.calculateTaskUserTotalDuration");
@@ -437,33 +220,6 @@ public class WorkRepository extends GenericRepository {
             log.error("LOG00700:", e);
         }
         return 0.0;
-        /*
-        double result = 0.0;
-        try {
-            Connection connection = database.getConnection();
-            PreparedStatement stmt = connection.prepareStatement("SELECT sum(workduration) sum FROM ( " +
-                            "SELECT yt.year, yt.month, yt.day, yt.created, yt.workduration, yt.taskuuid, yt.useruuid " +
-                            "FROM work yt INNER JOIN( " +
-                            "SELECT uuid, day, month, year, workduration, taskuuid, useruuid, max(created) created " +
-                            "FROM work WHERE taskuuid LIKE ? AND useruuid LIKE ? " +
-                            "GROUP BY day, month, year) ss " +
-                            "ON yt.month = ss.month AND yt.year = ss.year AND yt.day = ss.day AND yt.created = ss.created " +
-                            "AND yt.taskuuid = ss.taskuuid AND yt.useruuid = ss.useruuid " +
-                            ") we;",
-                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            stmt.setString(1, taskUUID);
-            stmt.setString(2, userUUID);
-            ResultSet resultSet = stmt.executeQuery();
-            resultSet.next();
-            result = resultSet.getDouble("sum");
-            resultSet.close();
-            stmt.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-        */
     }
 
     @Override
@@ -485,22 +241,6 @@ public class WorkRepository extends GenericRepository {
         } catch (Exception e) {
             log.error("LOG00710:", e);
         }
-        /*
-        Connection connection = database.getConnection();
-        PreparedStatement stmt = connection.prepareStatement("INSERT INTO work (uuid, day, month, year, taskuuid, useruuid, workduration, created) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-        stmt.setString(1, UUID.randomUUID().toString());
-        stmt.setInt(2, jsonNode.get("day").asInt());
-        stmt.setInt(3, jsonNode.get("month").asInt());
-        stmt.setInt(4, jsonNode.get("year").asInt());
-        stmt.setString(5, jsonNode.get("taskuuid").asText());
-        stmt.setString(6, jsonNode.get("useruuid").asText());
-        stmt.setInt(7, jsonNode.get("workduration").asInt());
-        stmt.setTimestamp(8, Timestamp.from(Instant.now()));
-        stmt.executeUpdate();
-        stmt.close();
-        connection.close();
-        System.out.println("saved!");
-        */
     }
 
     @Override
