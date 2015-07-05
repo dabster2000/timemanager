9c2b73350ddfda78d716d734de5f8d8d0463b779
hans <hans.lassen@trustworks.dk>
2015-07-05 15:18:56 +0200

    Fixed problem using database

--- src/main/java/dk/trustworks/timemanager/persistence/WeekRepository.java
+++ src/main/java/dk/trustworks/timemanager/persistence/WeekRepository.java
@@ -6,7 +6,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -34,23 +33,24 @@ public class WeekRepository extends GenericRepository {
                     .executeAndFetchTable().asList());
         } catch (Exception e) {
             log.error("LOG00570:", e);
+            throw new RuntimeException(e);
         }
-        return new ArrayList<>();
     }
 
     public List<Map<String, Object>> findByWeekNumberAndYearAndUserUUIDOrderBySortingAsc(int weekNumber, int year, String userUUID) {
         log.debug("WeekRepository.findByWeekNumberAndYearAndUserUUIDOrderBySortingAsc");
         log.debug("weekNumber = [" + weekNumber + "], year = [" + year + "], userUUID = [" + userUUID + "]");
         try (org.sql2o.Connection con = database.open()) {
-            return getEntitiesFromMapSet(con.createQuery("SELECT * FROM week w WHERE w.weeknumber = :weeknumber AND w.year = :year AND w.useruuid LIKE :useruuid ORDER BY sorting ASC")
+            List<Map<String, Object>> result = getEntitiesFromMapSet(con.createQuery("SELECT * FROM week w WHERE w.weeknumber = :weeknumber AND w.year = :year AND w.useruuid LIKE :useruuid ORDER BY sorting ASC")
                     .addParameter("weeknumber", weekNumber)
                     .addParameter("year", year)
                     .addParameter("useruuid", userUUID)
                     .executeAndFetchTable().asList());
+            return result;
         } catch (Exception e) {
             log.error("LOG00580:", e);
+            throw new RuntimeException(e);
         }
-        return new ArrayList<>();
     }
 
     @Override
